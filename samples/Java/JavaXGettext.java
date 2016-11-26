b848871eb5a3ea61dc284f0379de76b39374cdcc
Guido Flohr <guido.flohr@cantanea.com>
2016-11-26 21:43:08 +0200

    working scanner written in Java

--- samples/Java/JavaXGettext.java
+++ samples/Java/JavaXGettext.java
@@ -1,3 +1,4 @@
+import java.io.*;
 import java.util.*;
 import org.perl.inline.java.*;
 
@@ -6,13 +7,23 @@ class JavaXGettext extends InlineJavaPerlCaller {
     }
 
     public void readFile(String filename) 
-            throws InlineJavaException, InlineJavaPerlException {
-        CallPerlStaticMethod("Locale::XGettext::Callbacks", 
-                             "addEntry", 
-                             new Object [] {
-                                 "msgid", "Hello, world!",
-                                 "reference", filename + ":2304",
-                             }, 
-                             Integer.class);
+            throws InlineJavaException, InlineJavaPerlException,
+                   FileNotFoundException, IOException {
+        BufferedReader r = new BufferedReader(new FileReader(filename));
+        int lineno = 0;
+
+        for(String line; (line = r.readLine()) != null; ) {
+            ++lineno;
+
+            if (line.equals(""))
+                continue;
+            CallPerlStaticMethod("Locale::XGettext::Callbacks", 
+                                 "addEntry", 
+                                 new Object [] {
+                                     "msgid", line + "\n",
+                                     "reference", filename + ':' + lineno,
+                                 }, 
+                                 Integer.class);
+        }
     }
 }
