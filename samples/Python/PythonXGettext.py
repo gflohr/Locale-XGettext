869acfd0e4255b6c0fe10626a310e648d7ec5575
Guido Flohr <guido.flohr@cantanea.com>
2017-04-17 17:02:30 +0200

    python implementation

---
+++ samples/Python/PythonXGettext.py
@@ -0,0 +1,16 @@
+class PythonXGettext:
+    def readFile(self, xgettext, filename):
+        with open(filename) as f:
+            for line in f:
+                # You don't have to check that the line is empty.  The
+                # PO header gets added after input has been processed.
+                xgettext.addEntry({'msgid': line});
+
+    # Optional methods.
+    #def extractFromNonFiles(self, xgettext):
+
+    def defaultKeywords(self, xgettext):
+        return [
+                   ['gettext', '1'],
+                   ['ngettext', '1', '2']
+               ];
